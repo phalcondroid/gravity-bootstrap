0a77c539306a44938dd3cec5eff1ad2912487b6f
Julián Molina <julian.molina@brainz.co>
2017-02-20 18:02:32 -0500

    update class to modal generation

---
+++ Modal.ts
@@ -0,0 +1,136 @@
+/// <reference path="./definitions/jquery.d.ts"/>
+/// <reference path="./definitions/gravity.d.ts"/>
+
+namespace Gravity
+{
+    /**
+     *
+     * @type
+     */
+    export class Modal extends View.Div
+    {
+        /**
+         *
+         */
+        private modalDialog;
+
+        /**
+         *
+         */
+        private content;
+
+        /**
+         *
+         */
+        private body;
+
+        /**
+         *
+         */
+        private header;
+
+        /**
+         *
+         */
+        private footer;
+
+        /**
+         *
+         */
+        public initialize()
+        {
+            this.attr({
+                "class"    : "modal fade",
+                "tabindex" : "-1",
+                "role"     : "dialog",
+                "aria-labelledby" : "myModalLabel"
+            });
+            this.setDefault();
+        }
+
+        /**
+         *
+         */
+        private setDefault()
+        {
+            this.modalDialog = new View.Div(this.context);
+            this.modalDialog.attr({
+                "class" : "modal-dialog",
+                "role"  : "document"
+            });
+
+            this.content = new View.Div(this.context);
+            this.content.attr({
+                "class" : "modal-content"
+            });
+
+            this.header = new View.Div(this.context);
+            this.header.class("modal-header");
+
+            this.body   = new View.Div(this.context);
+            this.body.class("modal-body");
+
+            this.footer = new View.Div(this.context);
+            this.footer.class("modal-footer");
+
+            this.modalDialog.append(
+                this.content
+            );
+            this.append(
+                this.modalDialog
+            );
+        }
+
+        /**
+         *
+         */
+        public setHeader(title)
+        {
+            var closeButton = new View.Div(this.context);
+            closeButton.attr({
+                "class" : "close",
+                "type"  : "button",
+                "data-dismiss" : "modal",
+                "aria-label"   : "Close"
+            });
+            var span = new View.Span(this.context);
+            span.attr("aria-hidden", "true");
+            span.setInnerHtml("&times");
+            closeButton.append(span);
+
+            if (title != "") {
+                var titleHeader = new View.H4(this.context);
+                titleHeader.class("modal-title");
+                titleHeader.append(title);
+            }
+            return this;
+        }
+
+        /**
+         *
+         */
+        public setBody(body)
+        {
+            this.body.append(body);
+            return this;
+        }
+
+        /**
+         *
+         */
+        public setFooter(footer)
+        {
+            this.footer.append(footer);
+            return this;
+        }
+
+        /**
+         *
+         */
+        public launch()
+        {
+            $(this.element).modal("show");
+            return this;
+        }
+    }
+}
